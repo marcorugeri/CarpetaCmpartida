c2c8e03c909bf7800b02ceea669d5be85bea2c77
marco rugeri <marco.rugeri@hotmail.com>
2022-01-11 20:59:06 -0300

    update

---
+++ test.java
@@ -0,0 +1,17 @@
+package app.java;
+
+public class test {
+
+	public static void main(String[] args) {
+		 
+		 int a = 1;
+		 if (a > 0) {
+		 	System.out.println ("A es verdadero");
+		 	
+		 	System.out.println ("A es verdadero otra vez");
+
+		 } else {
+		 	System.out.println ("A es falso");
+		 }
+}
+}
